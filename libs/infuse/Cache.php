4e92f6ee8f057009febb43df8f5d4c57f6e0b416
jaredtking <jared@nfuseweb.com>
2013-09-05 20:49:11 -0500

    version bump

--- libs/infuse/Cache.php
+++ libs/infuse/Cache.php
@@ -4,7 +4,7 @@
  * @package infuse\libs
  * @author Jared King <j@jaredtking.com>
  * @link http://jaredtking.com
- * @version 0.1.15
+ * @version 0.1.15.1
  * @copyright 2013 Jared King
  * @license MIT
  */
