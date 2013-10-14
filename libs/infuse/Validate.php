5004f36b5795944b2f076167aab6022cb772c194
jaredtking <jared@nfuseweb.com>
2013-10-13 21:30:29 -0500

    version bump

--- libs/infuse/Validate.php
+++ libs/infuse/Validate.php
@@ -4,7 +4,7 @@
  * @package infuse\libs
  * @author Jared King <j@jaredtking.com>
  * @link http://jaredtking.com
- * @version 0.1.15.4
+ * @version 0.1.15.5
  * @copyright 2013 Jared King
  * @license MIT
  */
