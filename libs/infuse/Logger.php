dc867f8152af525c65ef1b227bd3d9a327802245
jaredtking <jared@nfuseweb.com>
2013-11-07 11:33:47 -0600

    version bump

--- libs/infuse/Logger.php
+++ libs/infuse/Logger.php
@@ -4,7 +4,7 @@
  * @package infuse\libs
  * @author Jared King <j@jaredtking.com>
  * @link http://jaredtking.com
- * @version 0.1.16.1
+ * @version 0.1.16.3
  * @copyright 2013 Jared King
  * @license MIT
  */
