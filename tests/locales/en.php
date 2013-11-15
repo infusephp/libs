4a6d5c23e2dad8114114e924f1dac035c37670b1
jaredtking <jared@nfuseweb.com>
2013-11-15 09:41:59 -0600

    version bump

--- tests/locales/en.php
+++ tests/locales/en.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @package infuse\libs
+ * @author Jared King <j@jaredtking.com>
+ * @link http://jaredtking.com
+ * @version 0.1.17
+ * @copyright 2013 Jared King
+ * @license MIT
+ */
+
 return array(
 	'phrases' => array(
 		'test_phrase' => 'This is a test',
