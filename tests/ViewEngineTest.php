a8423cb641f91ab60ab15be50e215fa0696c710d
Jared King <j@jaredtking.com>
2014-01-22 10:03:27 -0600

    improved viewengine tests

---
+++ tests/ViewEngineTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @package infuse\libs
+ * @author Jared King <j@jaredtking.com>
+ * @link http://jaredtking.com
+ * @version 0.1.18
+ * @copyright 2013 Jared King
+ * @license MIT
+ */
+
+use infuse\ViewEngine;
+
+class ViewEngineTest extends \PHPUnit_Framework_TestCase
+{
+	public function testTodo()
+	{
+		$engine = ViewEngine::engine();
+		
+        $this->markTestIncomplete(
+          'This test has not been implemented yet.'
+        );
+	}
+}
