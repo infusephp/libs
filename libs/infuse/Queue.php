5d2d6508448d43865064c78673f4c3bdaa276fb7
Jared King <j@jaredtking.com>
2014-05-23 22:55:44 -0500

    version bump -> v0.1.21.1

--- libs/infuse/Queue.php
+++ libs/infuse/Queue.php
@@ -4,7 +4,7 @@
  * @package infuse\libs
  * @author Jared King <j@jaredtking.com>
  * @link http://jaredtking.com
- * @version 0.1.21
+ * @version 0.1.21.1
  * @copyright 2014 Jared King
  * @license MIT
  */
