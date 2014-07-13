f3e9caddc2eb80d6bf00103f9dc89e6967f5acb5
Jared King <j@jaredtking.com>
2014-07-13 09:38:01 -0500

    version bump

--- libs/infuse/Util.php
+++ libs/infuse/Util.php
@@ -4,7 +4,7 @@
  * @package infuse\libs
  * @author Jared King <j@jaredtking.com>
  * @link http://jaredtking.com
- * @version 0.1.21.1
+ * @version 0.1.23
  * @copyright 2014 Jared King
  * @license MIT
  */
