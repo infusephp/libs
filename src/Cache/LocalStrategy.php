46920eff4fc8942cd16a35e69c1691566c793d47
Jared King <j@jaredtking.com>
2014-09-29 11:33:47 -0500

    version bump -> 0.2.2

--- src/Cache/LocalStrategy.php
+++ src/Cache/LocalStrategy.php
@@ -4,7 +4,7 @@
  * @package infuse\libs
  * @author Jared King <j@jaredtking.com>
  * @link http://jaredtking.com
- * @version 0.2.1
+ * @version 0.2.2
  * @copyright 2014 Jared King
  * @license MIT
  */
