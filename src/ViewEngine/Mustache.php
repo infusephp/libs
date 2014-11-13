2978d7b7e1c760ecf1a559c57165760b2db645b4
Jared King <j@jaredtking.com>
2014-11-13 14:44:29 -0600

    added mustache view engine

---
+++ src/ViewEngine/Mustache.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @package infuse\libs
+ * @author Jared King <j@jaredtking.com>
+ * @link http://jaredtking.com
+ * @version 0.2.2
+ * @copyright 2014 Jared King
+ * @license MIT
+ */
+
+namespace infuse\ViewEngine;
+
+use infuse\ViewEngine;
+use infuse\View;
+use Mustache_Engine;
+
+class Mustache extends ViewEngine
+{
+    private $viewsDir = 'views';
+    private $mustache;
+
+    const EXTENSION = '.mustache';
+
+    /**
+	 * Creates a new Mustache ViewEngine
+	 *
+	 * @param string $viewsDir optional dir containing templates
+	 */
+    public function __construct($viewsDir = false)
+    {
+        if ($viewsDir)
+            $this->viewsDir = $viewsDir;
+    }
+
+    public function renderView(View $view)
+    {
+        $mustache = $this->mustache();
+
+        // determine the full template name
+        $template = $view->template();
+
+        // add extension if left off
+        $len = strlen(self::EXTENSION);
+        if(substr($template, -$len, $len) != self::EXTENSION)
+            $template .= self::EXTENSION;
+
+        // compute full template path
+        $fullPath = $this->viewsDir . '/' . $template;
+
+        // assign global and view parameters
+        $parameters = array_replace($this->getGlobalParameters(), $view->getParameters());
+
+        // now let mustache do its thing
+        return $mustache->render(file_get_contents($fullPath), $parameters);
+    }
+
+    /**
+	 * Gets (and creates) a Mustache instance
+	 *
+	 * @return Mustache
+	 */
+    public function mustache()
+    {
+        if (!$this->mustache) {
+            $this->mustache = new Mustache_Engine();
+        }
+
+        return $this->mustache;
+    }
+}
---
+++ tests/ViewEngine/MustacheTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @package infuse\libs
+ * @author Jared King <j@jaredtking.com>
+ * @link http://jaredtking.com
+ * @version 0.2.2
+ * @copyright 2014 Jared King
+ * @license MIT
+ */
+
+use infuse\ViewEngine;
+use infuse\View;
+
+class MustacheViewEngineTest extends \PHPUnit_Framework_TestCase
+{
+    public static $engine;
+
+    public static function setUpBeforeClass()
+    {
+        self::$engine = new ViewEngine\Mustache(__DIR__ . '/views');
+    }
+
+    public function testAssetUrl()
+    {
+        $this->assertEquals(self::$engine, self::$engine->setAssetBaseUrl('http://localhost'));
+        $this->assertEquals('http://localhost/test', self::$engine->asset_url('/test'));
+
+        $this->assertEquals(self::$engine, self::$engine->setAssetMapFile(__DIR__ . '/static_assets.json'));
+
+        $this->assertEquals('http://localhost/img/logo.2v80s34k.png', self::$engine->asset_url('/img/logo.png'));
+        $this->assertEquals('http://localhost/test', self::$engine->asset_url('/test'));
+    }
+
+    public function testGlobalParameters()
+    {
+        self::$engine->setGlobalParameters(['test' => true, 'test2' => 'blah']);
+        self::$engine->setGlobalParameters(['test' => 'overwrite']);
+
+        $this->assertEquals(['test'=>'overwrite', 'test2'=>'blah'], self::$engine->getGlobalParameters());
+    }
+
+    public function testMustache()
+    {
+        $engine = new ViewEngine\Mustache('view');
+        $this->assertInstanceOf('Mustache_Engine', $engine->mustache());
+    }
+
+    public function testRenderView()
+    {
+        $view = new View('test', ['to' => 'world']);
+
+        self::$engine->setGlobalParameters([
+            'to' => 'should_be_overwritten',
+            'greeting' => 'Hello']);
+
+        $this->assertEquals('Hello, world!', self::$engine->renderView($view));
+    }
+}
