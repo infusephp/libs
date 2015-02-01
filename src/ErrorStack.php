e48ad67441056da710aff3622c6ee480f065b1ff
Jared King <j@jaredtking.com>
2015-02-01 09:49:48 -0600

    moved query builder to jaredtking/jaqb

--- src/ErrorStack.php
+++ src/ErrorStack.php
@@ -4,7 +4,7 @@
  * @package infuse\libs
  * @author Jared King <j@jaredtking.com>
  * @link http://jaredtking.com
- * @copyright 2014 Jared King
+ * @copyright 2015 Jared King
  * @license MIT
  */
 
